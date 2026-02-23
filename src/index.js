9de115d2df615835e1c138cad21bbd9d21d9cb61
Fi Sktk Admin <fisktk@example.com>
2026-02-23 21:41:48 +0200

    Configure backend for Vercel deployment

--- src/index.js
+++ src/index.js
@@ -367,6 +367,10 @@ app.post('/reset-password', async (req, res) => {
 });
 
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+if (process.env.NODE_ENV !== 'production') {
+  app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
+}
+
+module.exports = app;
